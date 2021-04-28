8d4393e89f4a1830e3356a11718854e2a675ef09
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-04-28 16:24:44 -0500

    fix the style problem

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Print number and list of arguments."""
-    import sys
+   import sys
 
-    count = len(sys.argv) - 1
-    if count == 0:
+   count = len(sys.argv) - 1
+   if count == 0:
         print("0 argunments.")
-    elif count == 1:
+   elif count == 1:
         print("1 argunment:")
-    else:
+   else:
         print("{} arguments:".format(count))
-    for index in range(count):
+   for index in range(count):
         print("{}: {}".format(index + 1, sys.argv[index + 1]))
