d8f62133bf393c066874a108bb19ffd1acf696f3
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-04-27 22:34:36 -0500

    all posible diferent combinations of 2 digit

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for number1 in range(0, 10):
+	for number2 in range(0, 10):
+		if number1 < number2 and number1 != 8:
+			print("{:d}{:d}".format(number1, number2), end=', ')
+		if number1 < number2 and number1 == 8:
+			print("{:d}{:d}".format(number1, number2))
+
