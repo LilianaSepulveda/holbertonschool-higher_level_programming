1b35880e878832e577195867e1ffdfd814b8b484
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-04-27 23:56:16 -0500

    fix the identation spaces

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for digit1 in range(0, 10):
-	for digit2 in range(0, 10):
-		if digit1 < digit2 and digit1 != 8:
-			print("{:d}{:d}".format(digit1, digit2), end=', ')
-		if digit1 < digit2 and digit1 == 8:
-			print("{:d}{:d}".format(digit1, digit2))
+    for digit2 in range(0, 10):
+        if digit1 < digit2 and digit1 != 8:
+            print("{:d}{:d}".format(digit1, digit2), end=', ')
+        if digit1 < digit2 and digit1 == 8:
+            print("{:d}{:d}".format(digit1, digit2))
