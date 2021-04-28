daa331048d114a66dd29cce73d7e8beb1d627a97
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-04-27 23:53:12 -0500

    fix the identation spaces

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,11 +3,11 @@ import random
 number = random.randint(-10000, 10000)
 ldigit = number % 10
 if number < 0:
-	ldigit = (abs(number) % 10) * -1
+    ldigit = (abs(number) % 10) * -1
 print("Last digit of {} is {} and is ".format(number, ldigit), end='')
 if ldigit > 5:
-	print('greater than 5')
+    print('greater than 5')
 elif ldigit == 0:
-	print('0')
+    print('0')
 else:
-	print('less than 6 and not 0')
+    print('less than 6 and not 0')
