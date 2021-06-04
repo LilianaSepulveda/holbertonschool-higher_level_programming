67415e483daa1edf6a6a2ce7014d90b84edea1c7
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-06-04 12:48:53 -0500

    change the order of conditionals and return value

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,7 +6,7 @@
 def max_integer(my_list=[]):
     my_list.sort()
 
-    if my_list == "":
-        return None
-    else:
+    if my_list:
         return my_list[-1]
+    else:
+        return None
