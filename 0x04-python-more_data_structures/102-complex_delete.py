db9a3b7c90047b1b65ee6cde6724e3de3beb08d5
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-06-05 12:43:57 -0500

    add if statementto value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-# function that deletes keys with a specific value in a dictionary
+# function deletes keys with a specific value in a dictionary
 
 
 def complex_delete(a_dictionary, value):
     while value in a_dictionary.values():
-        for key, value in a_dictionary.items():
-            del a_dictionary[key]
-            break
+        for k, v in a_dictionary.items():
+            if v == value:
+                del a_dictionary[k]
+                break
     return a_dictionary
