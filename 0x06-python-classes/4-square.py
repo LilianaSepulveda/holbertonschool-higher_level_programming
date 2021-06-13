daf15e4105d4306acfbc27cf889d51bb82470272
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-06-13 15:30:14 -0500

    access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""contains a class Square"""
+
+
+class Square:
+    """Define a square"""
+    def __init__(self, size=0):
+        """Initialize a new Square
+        parameters:
+            size (int): size of the new square
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """Is to get it back"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """It's to configure it"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """return the current square area"""
+        return self.__size ** 2
