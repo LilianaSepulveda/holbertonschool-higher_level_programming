efbedd182fc6a9f5bcaf8243c005b6b2ef3d4e71
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-06-14 10:16:46 -0500

    coordinates of square, add else conditional and identation

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -50,12 +50,12 @@ class Square:
         """print the square with the # character"""
         if self.__size == 0:
             print("")
-
-        for i in range(0, self.__position[1]):
-            print("")
-        for i in range(0, self.__size):
-            for j in range(0, self.__position[0]):
-                print(" ", end="")
-            for k in range(0, self.__size):
-                print("#", end="")
-            print("")
+        else:
+            for i in range(self.__position[1]):
+                print("")
+            for i in range(self.__size):
+                for j in range(self.__position[0]):
+                    print(" ", end="")
+                for k in range(self.__size):
+                    print("#", end="")
+                print("")
