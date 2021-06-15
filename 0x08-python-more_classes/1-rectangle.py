5d92cdd1b5fd5e13519965457563df06b31626fc
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-06-14 21:45:19 -0500

    change the order to the code

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,22 +10,8 @@ class Rectangle:
             width (int): width of the new rectangle.
             height (int): height of a new rectangle.
         """
-        self.__height = height
         self.__width = width
-
-    @property
-    def height(self):
-        """to get height back"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """It's to configure height"""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+        self.__height = height
 
     @property
     def width(self):
@@ -39,4 +25,20 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
+
         self.__width = value
+
+    @property
+    def height(self):
+        """to get height back"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """It's to configure height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
