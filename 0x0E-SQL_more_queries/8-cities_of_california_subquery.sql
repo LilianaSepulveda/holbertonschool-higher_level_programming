d7c484b704ecd4e4c3e5f3cd6c90557a351b009f
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-07-13 23:54:19 -0500

    Lists all cities of California in  hbtn_0d_usa

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,9 @@
+-- Lists all cities of California in  hbtn_0d_usa.
+
+SELECT id, name
+FROM cities
+WHERE state_id IN (
+    	SELECT id
+	  	FROM states
+	 	WHERE name = "California")
+ ORDER BY id ASC;
