ebef59f9b74243f5a1df8e3d883a0ce046d4892f
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-07-14 22:24:44 -0500

    import the database dump from hbtn_0d_tvshows, update

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,5 @@
--- mport the database dump from hbtn_0d_tvshows
+-- import the database dump from hbtn_0d_tvshows
+
 SELECT tv_genres.name AS 'gen',COUNT(tv_show_genres.genre_id) AS 'numshows' FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
 GROUP BY gen
