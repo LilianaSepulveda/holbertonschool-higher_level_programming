5bd359f831402a22cd0ee29b086b56107fd85299
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-07-14 22:27:44 -0500

    fix code, import the database dump

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- import the database dump from hbtn_0d_tvshows
 
-SELECT tv_genres.name AS 'gen',COUNT(tv_show_genres.genre_id) AS 'numshows' FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
+SELECT tv_genres.name AS 'gen', COUNT(*) AS 'numshows' FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY gen
 ORDER BY numshows DESC;
