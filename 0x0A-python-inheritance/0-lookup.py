dc4f98d9c52b223786f9da914f78472ea557adc9
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-07-23 12:08:50 -0500

    fix code

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,10 +4,4 @@
 
 def lookup(obj):
     """return a list of attributes and methods of a object"""
-
-    list_obj = []
-
-    for i in dir(obj):
-        list_obj.append(i)
-
-    return list_obj
+    return (dir(obj))
