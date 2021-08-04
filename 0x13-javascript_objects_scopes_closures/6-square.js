51e679ecfbe1f7a430b3722940391426f358ea19
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-08-03 23:25:15 -0500

    create an instance method charPrint in class Square

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+module.exports = class Square extends require('./5-square') {
+  charPrint (c) {
+    let printChar = c;
+    if (printChar === undefined) {
+      printChar = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(printChar.repeat(this.width));
+    }
+  }
+};
