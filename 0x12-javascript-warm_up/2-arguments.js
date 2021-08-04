ffebb5d9d8402edf44c0e2d5dc1cb2e7532df07c
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-08-04 10:32:57 -0500

    print a message depending of the number of argument passes

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const ArgPass = process.argv.lenght;
+if (ArgPass === 2) {
+  console.log('No argument');
+} else if (ArgPass === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
