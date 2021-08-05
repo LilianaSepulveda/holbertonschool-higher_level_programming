abad4d4cb3e4bad637457f05b54ba3e1fcce3ca4
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-08-04 21:49:35 -0500

    change the optional parameter, add encoding

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-const A = fs.readFileSync(process.argv[2], 'r');
-const B = fs.readFileSync(process.argv[3], 'r');
+const A = fs.readFileSync(process.argv[2], 'utf-8');
+const B = fs.readFileSync(process.argv[3], 'utf-8');
 fs.writeFileSync(process.argv[4], A + B);
