20c769f88442c3a76ae5bfd7577e6e886863b266
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-08-04 20:41:00 -0500

    fix identation, second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,8 +2,8 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-const ArgPass = process.argv.map(Number)
-  .slice(2, process.argv.length)
-  .sort((a, b) => a - b);
+  const ArgPass = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
   console.log(ArgPass[ArgPass.length - 2]);
 }
