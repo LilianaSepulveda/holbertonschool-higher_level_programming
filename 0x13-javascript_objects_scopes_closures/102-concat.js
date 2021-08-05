f7de5b2ac9c5e442bbbdb672001cf94b1d27c074
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-08-04 21:46:58 -0500

    change variable, concats 2 files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-const 1 = fs.readFileSync(process.argv[2], 'r');
-const 2 = fs.readFileSync(process.argv[3], 'r');
-fs.writeFileSync(process.argv[4], 1 + 2);
+const A = fs.readFileSync(process.argv[2], 'r');
+const B = fs.readFileSync(process.argv[3], 'r');
+fs.writeFileSync(process.argv[4], A + B);
