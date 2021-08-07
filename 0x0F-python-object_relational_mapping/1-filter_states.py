db47d5b0c564c3cc96ef5372d27f016218e818f0
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-08-07 17:18:06 -0500

    list all states with a name starting with Nfrom database

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module doc
+
+
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """it file dont permite to be executed whe import
+    """
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=argv[1],
+                           passwd=argv[2],
+                           db=argv[3],
+                           charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if (row[1][0] == "N"):
+            print(row)
+
+    cur.close()
+    conn.close()
