7df46692a32fe5cd96425f9705dc2d7b58ede80f
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-08-07 13:36:33 -0500

    fix identation, list all states from database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+# List all states from database hbtn_0e_0usa
+
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-	conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3], charset="utf8")
-	cur = conn.cursor()
-	cur.execute("SELECT * FROM states ORDER BY id ASC")
-	query_rows = cur.fetchall()
-	for states in query_rows:
-		print(states)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for states in query_rows:
+        print(states)
 
 cur.close()
-conn.close()
+db.close()
