462c6b34e39abbfda742f080ed5074ffd270af08
LilianaSepulveda <lilianasepulveda13@gmail.com>
2021-08-08 21:07:30 -0500

    print the state object with the name passed as argument from database

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# List all State objects with the name passed as argument
+"""
+Module doc
+
+
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == argv[4]).first()
+
+    if state is None:
+        print("Not found")
+    else:
+        print("{}".format(state.id))
+    session.close()
